6d7999af83ae7d2bdc18486f5717a59486b47ed1
UmeshManicum <98045016+UmeshManicum@users.noreply.github.com>
2023-04-23 17:42:21 -0400

    Update payment.py

--- App/models/payment.py
+++ App/models/payment.py
@@ -4,9 +4,7 @@ from datetime import date, time, datetime
 class Payment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False)
-    amount_owed = db.Column(db.Integer, default=500)
-    amount_paid = db.Column(db.Integer, nullable=False)
-    paid = db.Column(db.Boolean, default=False)
+    amount = db.Column(db.Integer, nullable=False)
     date = db.Column(db.DateTime(), default=datetime.utcnow)
 
     def __init__(self, amount):
