498e372538cdb083c4a3619edb14797aa8d05962
UmeshManicum <98045016+UmeshManicum@users.noreply.github.com>
2023-04-24 20:10:57 -0400

    Update activity.py

--- App/models/activity.py
+++ App/models/activity.py
@@ -4,15 +4,20 @@ class Activity(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title =  db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(255), nullable=False)
+    time_spent = db.Column(db.Integer, nullable=False)
+    calories = db.Column(db.Integer, nullable=False)
 
-
-    def __init__(self, title, description):
+    def __init__(self, title, description, time_spent, calories):
         self.title = title
         self.description = description
+        self.time_spent = time_spent
+        self.calories = calories
 
     def toJSON(self):
         return{
             'id': self.id,
             'title': self.title,
-            'description': self.description
+            'description': self.description,
+            'time_spent': self.time_spent,
+            'calories': self.calories
         }
